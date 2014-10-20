39927e461bf6a60fd3cb5fdb0b49ffa34acc5fa9
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2014-10-20 15:47:16 +0300

    Apparently working parser.

--- parse.py
+++ parse.py
@@ -1,59 +1,82 @@
-import re, os
+import re, os, datetime
 
-ifilenames = [s.strip() for s in os.popen("ls data/200[0-9]_[0-9][0-9]_[a-z]*/*.[0-9][0-9][0-9]").readlines()]
+file_shell_rex = "data/200[0-9]_[0-9][0-9]_[a-z]*/*.[0-9][0-9][0-9]"
+site_rex = re.compile("^\*SITE NUMBER: ([0-9]+)$")
+date_rex = re.compile("^DATE\s+([0-9]?[0-9])/([0-1]?[0-9])/([0-3]?[0-9])\s[0-9\s]+AVERAGE$")
+count_rex = re.compile("[12]?[0-9]\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+")
+totals_rex = re.compile("^TOTALS$")
+
+ifilenames = [s.strip() for s in os.popen("ls %s" % file_shell_rex).readlines()]
 
 def ilines(ifilenames):
     for ifilename in ifilenames:
-        for line in open(ifilename):
+        ifile = open(ifilename) # pypy requires explicit close
+        for line in ifile:
             if len(line)>2 and line[-1]=="\n": yield ifilename, line.strip()
-        yield None, 'eof'
+        ifile.close()
+        yield None, 'eof'        
                             
 def events(lines):
     for ifilename, line in lines:
         if line=="eof":
             yield 'eof', None
         else:
-            m = re.match("^\*SITE NUMBER: ([0-9]+)$", line)
+            m = re.match(site_rex, line)
             if m:
                 yield 'site', (int(m.groups()[0]), ifilename)
             else:
-                m = re.match("^DATE\s+ ([0-9][0-9]/[0-9][0-9]/[0-9][0-9])\s[0-9\s]+AVERAGE", line)
+                m = re.match(date_rex, line)
                 if m:
-                    yield 'date', m.groups()[0]
+                    yield 'date', m.groups()
                 else:
-                    m = re.match("[12]?[0-9]\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+", line)
+                    m = re.match(count_rex, line)
                     if m:
                         try:
                             fields = [int(i) if i != "-" else None for i in line.split()]
                             yield 'hourly', (fields[0], fields[1:8])
                         except ValueError:
                             yield 'error', line
-
+                    else:
+                        m = re.match(totals_rex, line)
+                        if m:
+                            yield 'eod', None
+                        
 def data_lines(evs):
-    errc = 0
+    errors = []
     for evt, info in evs:
         if evt=="error":
-            errc += 1
+            errors.append("format")
             continue
         elif evt=="eof":
-            date, site, hcount = None, None, 0
-        elif evt=="date":
-            date = info
-            hcount = 0
+            date, site, hdata = None, None, {}
         elif evt=="site":
             site, filename = info
-            yield site
+        elif evt=="date":
+            date = info
+            hours, hdata = set(xrange(24)), {}
         elif evt=="hourly":
-            if date==None or site==None: continue
+            if date==None or site==None:
+                continue
             else:
-                hcount += 1
-                if hcount>24:
-                    raise ValueError, "Oops, hcount goes over 24."
                 hour, counts = info
-                for iday, count in enumerate(counts):
-                    yield errc, site, date, iday, hour, count
-                
-            
-for line in data_lines(events(ilines(ifilenames))):
-    print line
-    
+                hours.remove(hour)
+                hdata[hour] = counts
+        elif evt=="eod":
+            if len(hours)==0:
+                for hour, counts in hdata.iteritems():
+                    for iday, count in enumerate(counts):
+                        yield errors, (site, date, iday, hour, count)
+            else:
+                errors.append("incomplete")
+            hdata = {}
+
+def date_vars(dtuple, i):
+    year, month, day = [int(i) for i in dtuple]
+    date = datetime.datetime(year+2000, month, day, 0, 0) + datetime.timedelta(int(i))
+    days = (date - datetime.datetime(2004, 1, 1, 0, 0)).days
+    return date.strftime("%Y-%m-%d"), days, date.strftime("%A"), date.timetuple().tm_yday
+
+
+print "site date day weekday yday hour count"
+for errors, (site, dtuple, iday, hour, count) in data_lines(events(ilines(ifilenames))):
+    print "%s %s %s %s %s %s %s" % ((site, ) + date_vars(dtuple, iday) + (hour, "NA" if count==None else count))
