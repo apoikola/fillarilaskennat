d4a27b6550e1276b613bda04cfe6bad85683515f
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2014-10-27 21:55:45 +0200

    Bugs with iday and start day (neg days) fixed, channel info added

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import re, os, datetime
 
 file_shell_rex = "data/200[0-9]_[0-9][0-9]_[a-z]*/*.[0-9][0-9][0-9]"
 site_rex = re.compile("^\*SITE NUMBER: ([0-9]+)$")
+channel_rex = re.compile("^\*CHANNEL\s*:\s+([0-9]+)\s+OF")
 date_rex = re.compile("^DATE\s+([0-9]?[0-9])/([0-1]?[0-9])/([0-3]?[0-9])\s[0-9\s]+AVERAGE$")
 count_rex = re.compile("[12]?[0-9]\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+\s+[0-9-]+")
 totals_rex = re.compile("^TOTALS$")
@@ -24,22 +25,26 @@ def events(lines):
             m = re.match(site_rex, line)
             if m:
                 yield 'site', (int(m.groups()[0]), ifilename)
-            else:
-                m = re.match(date_rex, line)
+	    else:
+                m = re.match(channel_rex, line)
                 if m:
-                    yield 'date', m.groups()
+                    yield 'channel', (int(m.groups()[0]), ifilename)
                 else:
-                    m = re.match(count_rex, line)
+                    m = re.match(date_rex, line)
                     if m:
-                        try:
-                            fields = [int(i) if i != "-" else None for i in line.split()]
-                            yield 'hourly', (fields[0], fields[1:8])
-                        except ValueError:
-                            yield 'error', line
+                        yield 'date', m.groups()
                     else:
-                        m = re.match(totals_rex, line)
+                        m = re.match(count_rex, line)
                         if m:
-                            yield 'eod', None
+                            try:
+                                fields = [int(i) if i != "-" else None for i in line.split()]
+                                yield 'hourly', (fields[0], fields[1:8])
+                            except ValueError:
+                                yield 'error', line
+                        else:
+                            m = re.match(totals_rex, line)
+                            if m:
+                                yield 'eod', None
                         
 def data_lines(evs):
     errors = []
@@ -51,6 +56,8 @@ def data_lines(evs):
             date, site, hdata = None, None, {}
         elif evt=="site":
             site, filename = info
+        elif evt=="channel":
+            channel = info[0]
         elif evt=="date":
             date = info
             hours, hdata = set(xrange(24)), {}
@@ -65,18 +72,19 @@ def data_lines(evs):
             if len(hours)==0:
                 for hour, counts in hdata.iteritems():
                     for iday, count in enumerate(counts):
-                        yield errors, (site, date, iday, hour, count)
+                        yield errors, (site, channel, date, iday, hour, count)
             else:
                 errors.append("incomplete")
             hdata = {}
 
-def date_vars(dtuple, i):
+def date_vars(dtuple, iday):
     year, month, day = [int(i) for i in dtuple]
-    date = datetime.datetime(year+2000, month, day, 0, 0) + datetime.timedelta(int(i))
-    days = (date - datetime.datetime(2004, 1, 1, 0, 0)).days
+    date = datetime.datetime(year+2000, month, day, 0, 0) + datetime.timedelta(int(iday))
+    days = (date - datetime.datetime(2003, 12, 1, 0, 0)).days
     return date.strftime("%Y-%m-%d"), days, date.strftime("%A"), date.timetuple().tm_yday
 
 
-print "site date day weekday yday hour count"
-for errors, (site, dtuple, iday, hour, count) in data_lines(events(ilines(ifilenames))):
-    print "%s %s %s %s %s %s %s" % ((site, ) + date_vars(dtuple, iday) + (hour, "NA" if count==None else count))
+print "site channel date day weekday yday hour count"
+for errors, (site, channel, dtuple, iday, hour, count) in data_lines(events(ilines(ifilenames))):
+    print "%s %s %s %s %s %s %s %s" % ((site, channel) + date_vars(dtuple, iday) + (hour, "NA" if count==None else count))
+
