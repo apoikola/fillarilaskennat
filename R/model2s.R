3ebc21b64d17d9918b73504e633c15e1334bd530
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2014-11-08 21:09:13 +0200

    With model matrix. Takes 20–60 mins to fit.

--- R/model2s.R
+++ R/model2s.R
@@ -4,14 +4,23 @@ library(ggplot2)
 
 d <- (function () { source("R/model-data.R", local=T); environment()})()$d
 
+# Weak penalization.
+Xmain <- model.matrix(~ 0 + main.site + I(year/10) + I(year/10):main.site + 
+                        holiday + weekday + july + near.july +
+                        I(snow/50) + I(rrday/50) + I(rrday1/50) + I((tmax-tmin)/10) +
+                        I(snow==0) + I(rrday==0) + I(rrday1==0) +
+                        earth.phase, d) 
+
+# Penalized interactions will be in a separate matrix.
+
 
 m <- stan_model("R/model2s.stan")
 s <- sampling(m, chains=1, iter=500, seed=3, 
               data=list(
-                N=nrow(d),  
+                N=nrow(d), Nmain=ncol(Xmain), Xmain=Xmain,
                 count=d$count, tday=d$tday),
               #pars=c("ktemp", "iphi", "intercept"),
-              refresh=10)#, control=list(metric="diag_e", stepsize_jitter=0.3))
+              refresh=1, control=list(metric="diag_e", stepsize_jitter=0.3))
 plot(s)
 traceplot(s)
-traceplot(s, inc_warmup=F)
+traceplot(s, c("iphi", "ktemp"), inc_warmup=F)
--- R/model2s.stan
+++ R/model2s.stan
@@ -1,18 +1,21 @@
 data {
-  int<lower=0> N; 
+  int<lower=0> N; int<lower=0> Nmain;
   int<lower=0> count[N];
   vector[N] tday;
+  matrix[N,Nmain] Xmain;
 }
 parameters {
   real<lower=0> iphi;
   real ktemp;
+  vector[Nmain] kmain;
   real intercept;
 }
 model {
   vector[N] t; 
+  intercept ~ normal(6, 6);
   ktemp ~ normal(0, 2);
-  intercept ~ normal(6, 3);
-  t <- ktemp*tday/10 + intercept;
+  kmain ~ normal(0, 2); // Cauchy here.
+  t <- ktemp*tday/10 + Xmain*kmain + intercept;
   iphi ~ normal(0, 1.0);
   count ~ neg_binomial_2_log(t, 1/iphi);
 }
